b88d974ed7a6dff829d0e04ba052fe003f91b535
James Black <james@epijim.uk>
2017-07-20 19:58:54 +0200

    first commit

---
+++ summaryspecs.R
@@ -0,0 +1,12 @@
+#' Add together two numbers
+#'
+#' @param x The dataframe
+#' @param y A number
+#' @return The sum of \code{x} and \code{y}
+#' @examples
+#' add(1, 1)
+#' add(10, 1)
+summaryspecs <- function(x) {
+  # data checks
+    # is it a dataframe?
+}
